6da94d89b44db8399dcb25a7c86aa56049d4653d
aglim-benli <aglim.benli@student.hogent.be>
2022-10-21 18:51:22 +0200

    Update VirtualMachine.cs

--- src/Shared/VirtualMachine.cs
+++ src/Shared/VirtualMachine.cs
@@ -1,4 +1,20 @@
-﻿public class VirtualMachine
-{
+﻿namespace Shared;
 
+public class VirtualMachine
+{
+    public string? Name { get; set; }
+    public string? Hostname { get; set; }
+    public string? FQDN { get; set; }
+    public bool IsHighlyAvailable { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public bool IsCreated { get; set; }
+    public bool IsActive { get; set; }
+    public int Cpu { get; set; }
+    public int Ram { get; set; }
+    public int Storage { get; set; }
+    public ETemplate Template { get; set; }
+    public EMode Mode { get; set; }
+    public EDay AvailableDays { get; set; }
+    public EBackupFrequency BackupFrequency { get; set; }
 }
