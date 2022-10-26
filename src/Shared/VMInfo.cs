9e28211ac15d3ef28d7ec6e3ce081ceabc9b09e3
JelleDelporte <71758797+JelleDelporte@users.noreply.github.com>
2022-10-26 18:56:52 +0200

    VM Aanvragen

---
+++ src/Shared/VMInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared;
+
+public class VMInfo
+{
+    [Required]
+    [StringLength(10, ErrorMessage = "Naam is te lang")]
+    public string? Name { get; set; }
+    [Required]
+    public string? HostName { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; }
+    [Required]
+    public DateTime EndDate { get; set; }
+    [Required]
+    public bool IsHighAvailable { get; set; }
+    [Required]
+    public string? FQDN { get; set; }
+    [Required]
+    public int[]? Poorten { get; set; }
+    [Required]
+    public EBackupFrequency EBackupFrequency { get; set; }
+    [Required]
+    public EDay[] Availability { get; set; }
+
+}
--- src/Shared/VirtualMachineRequest.cs
+++ src/Shared/VirtualMachineRequest.cs
@@ -1,6 +1,16 @@
-﻿namespace Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared;
 
 public class VirtualMachineRequest
 {
-    public DateTime Date { get; set; }
+    //[Required]
+    //public DateTime Date { get; set; }
+    [Required]
+    [StringLength(10, ErrorMessage = "Naam is te lang")]
+    [MinLength(10, ErrorMessage = "Te klein")]
+    public string? Reason { get; set; }
+
+    //    [Required]
+    //    public VMInfo? VMInfo { get; set; }
 }
