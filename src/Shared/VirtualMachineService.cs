360be58bb263296eba3a8740908aaedc870cadd9
Jelle Delporte <71758797+JelleDelporte@users.noreply.github.com>
2022-10-31 15:57:17 +0100

    VM info weg

--- src/Shared/VirtualMachineService.cs
+++ src/Shared/VirtualMachineService.cs
@@ -36,26 +36,29 @@ namespace Shared
             var vm1 = new VirtualMachine
             {
                 Id = 1,
-                VMInfo = new()
-                {
-                    Name = "VM-IT-1",
-                    HostName = "VM_JN58CE_2354",
-                    FQDN = "TBD",
-                    IsHighAvailable = true,
-                    StartDate = new DateTime(2022, 01, 15),
-                    EndDate = new DateTime(2023, 02, 14),
-                    Template = Template.TEMPLATES[ETemplate.ArtificialIntelligence],
-                    Availability = (EDay)((int)EDay.Monday + (int)EDay.Tuesday + (int)EDay.Wednesday + (int)EDay.Thursday + (int)EDay.Friday),
-                    EBackupFrequency = EBackupFrequency.Daily
-                },
-                IsActive = true
+                Name = "VM-IT-1",
+                HostName = "VM_JN58CE_2354",
+                FQDN = "TBD",
+                IsHighAvailable = true,
+                StartDate = new DateTime(2022, 01, 15),
+                EndDate = new DateTime(2023, 02, 14),
+                Template = Template.TEMPLATES[ETemplate.ArtificialIntelligence],
+                Availability = (EDay)((int)EDay.Monday + (int)EDay.Tuesday + (int)EDay.Wednesday + (int)EDay.Thursday + (int)EDay.Friday),
+                EBackupFrequency = EBackupFrequency.Daily,
+                IsActive = true,
+                CreateDate = DateTime.Now,
+                Poorten = "Poort 1, poort2",
+                Host = "host123pt-45f",
+                CPU = 2,
+                RAM = 5,
+                Storage = 920,
+                Mode = EMode.IaaS
             };
             
             var vm2 = new VirtualMachine
             {
                 Id = 2,
-                VMInfo = new()
-                {
+
                     Name = "VM-IT-2",
                     HostName = "VM_GH35ZR_5436",
                     FQDN = "TBD",
@@ -64,9 +67,16 @@ namespace Shared
                     EndDate = new DateTime(2023, 03, 17),
                     Template = Template.TEMPLATES[ETemplate.Database],
                     Availability = (EDay)((int)EDay.Monday + (int)EDay.Tuesday + (int)EDay.Wednesday + (int)EDay.Thursday + (int)EDay.Friday + (int)EDay.Saturday + (int)EDay.Sunday),
-                    EBackupFrequency = EBackupFrequency.Weekly
-                },
-                IsActive = false
+                    EBackupFrequency = EBackupFrequency.Weekly,
+
+                IsActive = false,
+                CreateDate = DateTime.Now,
+                Poorten = "Poort 1, poort2",
+                Host = "host123pt-45f",
+                CPU = 2,
+                RAM = 5,
+                Storage = 920,
+                Mode = EMode.IaaS
             };
 
             // TODO more vms
