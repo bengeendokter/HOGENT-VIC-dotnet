98aad6118cd68ddf34dcf9855a2b51d1e725ef89
Mert Sahin <mert.sahin@student.hogent.be>
2022-11-11 20:09:26 +0100

    merge commit 2

--- src/Client/VirtualMachines/VirtualMachineRequestService.cs
+++ src/Client/VirtualMachines/VirtualMachineRequestService.cs
@@ -68,15 +68,9 @@ public class VirtualMachineRequestService : IVirtualMachineRequestService
         var request1 = new VirtualMachineRequest
         {
             Id = 1,
-<<<<<<< HEAD:src/Client/VirtualMachines/VirtualMachineRequestService.cs
-            Date = new DateTime(2022, 10, 25, 9, 15, 0),
-            StartDate = new DateTime(2022, 11, 15, 9, 15, 0),
-            EndDate = DateTime.MaxValue,
-=======
             Date = DatumCreator(1, 0).AddMonths(-1),
             StartDate = DatumCreator(1, 0),
             EndDate = DatumCreator(2, 0),
->>>>>>> analytics:src/Shared/VirtualMachineRequestService.cs
             Reason = "Virtual machine voor een bachlerproef onderzoek ivm AI.",
             ProjectNaam = "bachlerproef AI",
             Status = ERequestStatus.Accepted,
@@ -86,15 +80,10 @@ public class VirtualMachineRequestService : IVirtualMachineRequestService
         var request2 = new VirtualMachineRequest
         {
             Id = 2,
-<<<<<<< HEAD:src/Client/VirtualMachines/VirtualMachineRequestService.cs
-            Date = new DateTime(2022, 10, 23, 9, 15, 0),
-            StartDate = new DateTime(2022, 11, 05, 9, 15, 0),
-            EndDate = DateTime.MaxValue,
-=======
+
             Date = DatumCreator(1, 1).AddMonths(-1),
             StartDate = DatumCreator(1, 1),
             EndDate = DatumCreator(2, 1),
->>>>>>> analytics:src/Shared/VirtualMachineRequestService.cs
             Reason = "Virtual machine voor een DevOps opdracht.",
             ProjectNaam = "Opdracht",
             Status = ERequestStatus.Handled,
@@ -103,27 +92,17 @@ public class VirtualMachineRequestService : IVirtualMachineRequestService
         };
         var request3 = new VirtualMachineRequest
         {
-<<<<<<< HEAD:src/Client/VirtualMachines/VirtualMachineRequestService.cs
-            Id = 3, 
-            Date = new DateTime(2022, 10, 20, 9, 15, 0),
-            StartDate = new DateTime(2022, 11, 19, 9, 15, 0),
-            EndDate = DateTime.MaxValue,
-=======
             Id = 3,
             Date = DatumCreator(4, 4).AddMonths(-1),
             StartDate = DatumCreator(4, 4),
             EndDate = DatumCreator(4, 19),
->>>>>>> analytics:src/Shared/VirtualMachineRequestService.cs
             Reason = "Virtual machine voor iets online te zetten",
             ProjectNaam = "Online",
             Status = ERequestStatus.Denied,
             EmailAanvrager = "harld@test.be",
             NummerAanvrager = 0469569562
         };
-<<<<<<< HEAD:src/Client/VirtualMachines/VirtualMachineRequestService.cs
-=======
 
->>>>>>> analytics:src/Shared/VirtualMachineRequestService.cs
         _requests.Add(request1);
         _requests.Add(request2);
         _requests.Add(request3);
