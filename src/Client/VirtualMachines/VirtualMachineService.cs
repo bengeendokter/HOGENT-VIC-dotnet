13678bf07ec4860eab4d54d7d29db418796bfa25
Mert Sahin <mert.sahin@student.hogent.be>
2022-11-21 17:00:25 +0100

    voorspelling gebruikt vm data

--- src/Client/VirtualMachines/VirtualMachineService.cs
+++ src/Client/VirtualMachines/VirtualMachineService.cs
@@ -75,6 +75,43 @@ public class VirtualMachineService : IVirtualMachineService
         return Get(id);
     }
 
+    private DateTime DatumCreator(int maand, int dag)
+    {
+        int j = DateTime.Now.Year;
+        int m = DateTime.Now.Month;
+        int d = DateTime.Now.Day;
+
+        var dagen = DateTime.DaysInMonth(j, m);
+
+        int j1 = j;
+        int m1 = m;
+        int d1 = d;
+
+        if (d1 + dag > dagen)
+        {
+            d1 = (d1 + dag) % dagen;
+            m1 += 1;
+
+        }
+        else
+        {
+            d1 += dag;
+        }
+
+        if (m1 + maand > 12)
+        {
+            m1 = (m1 + maand) % 12;
+            j1 += 1;
+
+        }
+        else
+        {
+            m1 += maand;
+        }
+
+        return new DateTime(j1, m1, d1);
+    }
+
     private void SetDummyVirtualMachineList()
     {
         var vm1 = new VirtualMachineDto.Detail
@@ -84,8 +121,8 @@ public class VirtualMachineService : IVirtualMachineService
             HostName = "VM_JN58CE_2354",
             FQDN = "TBD",
             IsHighlyAvailable = true,
-            StartDate = new DateTime(2022, 01, 15),
-            EndDate = new DateTime(2023, 04, 14),
+            StartDate = DatumCreator(0, 0),
+            EndDate = DatumCreator(3, 1),
             Template = Template.TEMPLATES[ETemplate.ArtificialIntelligence],
             Availability = (EDay)((int)EDay.Monday + (int)EDay.Tuesday + (int)EDay.Wednesday + (int)EDay.Thursday + (int)EDay.Friday),
             BackupFrequency = EBackupFrequency.Daily,
@@ -106,8 +143,8 @@ public class VirtualMachineService : IVirtualMachineService
             HostName = "VM_GH35ZR_5436",
             FQDN = "TBD",
             IsHighlyAvailable = false,
-            StartDate = new DateTime(2022, 02, 16),
-            EndDate = new DateTime(2023, 03, 17),
+            StartDate = DatumCreator(3, 0),
+            EndDate = DatumCreator(5, 0),
             Template = Template.TEMPLATES[ETemplate.Database],
             Availability = (EDay)((int)EDay.Monday + (int)EDay.Tuesday + (int)EDay.Wednesday + (int)EDay.Thursday + (int)EDay.Friday + (int)EDay.Saturday + (int)EDay.Sunday),
             BackupFrequency = EBackupFrequency.Weekly,
@@ -121,9 +158,99 @@ public class VirtualMachineService : IVirtualMachineService
             Mode = EMode.IaaS
         };
 
-        // TODO more vms
+        var vm3 = new VirtualMachineDto.Detail
+        {
+            Id = 3,
+            Name = "VM-IT-3",
+            HostName = "VM_JN58CE_2354",
+            FQDN = "TBD",
+            IsHighlyAvailable = true,
+            StartDate = DatumCreator(4, 0),
+            EndDate = DatumCreator(6, 0),
+            Template = Template.TEMPLATES[ETemplate.ArtificialIntelligence],
+            Availability = (EDay)((int)EDay.Monday + (int)EDay.Tuesday + (int)EDay.Wednesday + (int)EDay.Thursday + (int)EDay.Friday),
+            BackupFrequency = EBackupFrequency.Daily,
+            IsActive = true,
+            CreatedAt = DateTime.Now,
+            Poorten = "Poort 1, poort2",
+            Host = "host123pt-45f",
+            CPU = 4,
+            RAM = 3,
+            Storage = 950,
+            Mode = EMode.IaaS
+        };
+
+        var vm4 = new VirtualMachineDto.Detail
+        {
+            Id = 4,
+            Name = "VM-IT-4",
+            HostName = "VM_GH35ZR_5436",
+            FQDN = "TBD",
+            IsHighlyAvailable = false,
+            StartDate = DatumCreator(0, 5),
+            EndDate = DatumCreator(3, 6),
+            Template = Template.TEMPLATES[ETemplate.Database],
+            Availability = (EDay)((int)EDay.Monday + (int)EDay.Tuesday + (int)EDay.Wednesday + (int)EDay.Thursday + (int)EDay.Friday + (int)EDay.Saturday + (int)EDay.Sunday),
+            BackupFrequency = EBackupFrequency.Weekly,
+            IsActive = false,
+            CreatedAt = DateTime.Now,
+            Poorten = "Poort 1, poort2",
+            Host = "host123pt-45f",
+            CPU = 2,
+            RAM = 5,
+            Storage = 920,
+            Mode = EMode.IaaS
+        };
+
+        var vm5 = new VirtualMachineDto.Detail
+        {
+            Id = 5,
+            Name = "VM-IT-5",
+            HostName = "VM_JN58CE_2354",
+            FQDN = "TBD",
+            IsHighlyAvailable = true,
+            StartDate = DatumCreator(3, 4),
+            EndDate = DatumCreator(5, 4),
+            Template = Template.TEMPLATES[ETemplate.ArtificialIntelligence],
+            Availability = (EDay)((int)EDay.Monday + (int)EDay.Tuesday + (int)EDay.Wednesday + (int)EDay.Thursday + (int)EDay.Friday),
+            BackupFrequency = EBackupFrequency.Daily,
+            IsActive = true,
+            CreatedAt = DateTime.Now,
+            Poorten = "Poort 1, poort2",
+            Host = "host123pt-45f",
+            CPU = 4,
+            RAM = 3,
+            Storage = 950,
+            Mode = EMode.IaaS
+        };
+
+        var vm6 = new VirtualMachineDto.Detail
+        {
+            Id = 6,
+            Name = "VM-IT-6",
+            HostName = "VM_GH35ZR_5436",
+            FQDN = "TBD",
+            IsHighlyAvailable = false,
+            StartDate = DatumCreator(4, 7),
+            EndDate = DatumCreator(6, 7),
+            Template = Template.TEMPLATES[ETemplate.Database],
+            Availability = (EDay)((int)EDay.Monday + (int)EDay.Tuesday + (int)EDay.Wednesday + (int)EDay.Thursday + (int)EDay.Friday + (int)EDay.Saturday + (int)EDay.Sunday),
+            BackupFrequency = EBackupFrequency.Weekly,
+            IsActive = false,
+            CreatedAt = DateTime.Now,
+            Poorten = "Poort 1, poort2",
+            Host = "host123pt-45f",
+            CPU = 2,
+            RAM = 5,
+            Storage = 920,
+            Mode = EMode.IaaS
+        };
 
         _vms.Add(vm1);
         _vms.Add(vm2);
+        _vms.Add(vm3);
+        _vms.Add(vm4);
+        _vms.Add(vm5);
+        _vms.Add(vm6);
     }
 }
