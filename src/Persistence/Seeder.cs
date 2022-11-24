dba17333ad6af15164d578b5aafbd1a5f146ff81
aglim-benli <aglim.benli@student.hogent.be>
2022-11-24 19:37:21 +0100

    Finish persistence setup

---
+++ src/Persistence/Seeder.cs
@@ -0,0 +1,20 @@
+using Domain.VirtualMachines;
+
+namespace Persistence;
+
+public class Seeder
+{
+    private readonly VicDbContext dbContext;
+
+    public Seeder(VicDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public void Seed()
+    {
+        SeedVirtualMachines();
+    }
+
+    private void SeedVirtualMachines() { }
+}
