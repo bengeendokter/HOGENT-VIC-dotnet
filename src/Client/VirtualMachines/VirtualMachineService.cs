8ca12d3d7657d3ded6c37be95cd41467ca7c7e8f
aglim-benli <aglim.benli@student.hogent.be>
2022-11-24 20:38:54 +0100

    Finish vm service in client

--- src/Client/VirtualMachines/VirtualMachineService.cs
+++ src/Client/VirtualMachines/VirtualMachineService.cs
@@ -1,26 +1,38 @@
-﻿namespace Client.VirtualMachines;
+﻿using System.Net.Http.Json;
+
+namespace Client.VirtualMachines;
 
 public class VirtualMachineService : IVirtualMachineService
 {
-    public VirtualMachineService() { }
+    private readonly HttpClient client;
+    private const string endpoint = "api/vm";
+
+    public VirtualMachineService(HttpClient client)
+    {
+        this.client = client;
+    }
 
     public async Task<List<VirtualMachineDto.Index>> GetIndexAsync()
     {
-        throw new NotImplementedException();
+        return await client.GetFromJsonAsync<List<VirtualMachineDto.Index>>(endpoint) ?? new();
     }
 
     public async Task<VirtualMachineDto.Detail> GetDetailAsync(int virtualMachineId)
     {
-        throw new NotImplementedException();
+        var response = await client.GetFromJsonAsync<VirtualMachineDto.Detail>(
+            $"{endpoint}/{virtualMachineId}"
+        );
+        return response!;
     }
 
     public async Task<int> CreateAsync(VirtualMachineDto.Mutate model)
     {
-        throw new NotImplementedException();
+        var response = await client.PostAsJsonAsync(endpoint, model);
+        return await response.Content.ReadFromJsonAsync<int>();
     }
 
     public async Task EditAsync(int virtualMachineId, VirtualMachineDto.Mutate model)
     {
-        throw new NotImplementedException();
+        await client.PutAsJsonAsync($"{endpoint}/{virtualMachineId}", model);
     }
 }
