06269bd63babff387a42e4dec2744ce46c94db57
CedrickLauwyck <cedrick.lauwyck@student.hogent.be>
2022-12-14 14:38:20 +0100

    Start virtualmachinereq

--- src/Shared/VirtualMachines/VirtualMachineReq.cs
+++ src/Shared/VirtualMachines/VirtualMachineReq.cs
@@ -6,6 +6,8 @@
         {
             public int Page { get; set; } = 1;
             public int PageSize { get; set; } = 25;
+            public string? SortBy { get; set; }
+            public string? Status { get; set; }
         }
     }
 }
