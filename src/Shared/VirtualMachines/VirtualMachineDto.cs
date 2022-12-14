7a68e3977f8bfaa9d8a929e67c0214f1dd4bf69e
Jelle Delporte <71758797+JelleDelporte@users.noreply.github.com>
2022-12-14 15:25:06 +0100

    bugfix in manage + flags in software enum

--- src/Shared/VirtualMachines/VirtualMachineDto.cs
+++ src/Shared/VirtualMachines/VirtualMachineDto.cs
@@ -72,7 +72,8 @@ public static class VirtualMachineDto
                 RuleFor(x => x.EndDate).GreaterThan(DateTime.Now).WithMessage("Startdatum moet in de toekomst liggen");
                 RuleFor(x => x.FQDN).NotEmpty().WithMessage("Dit veld is verplicht");
                 RuleFor(x => x.Template).NotNull().WithMessage("Kies een template of maak een nieuwe aan");
-                RuleFor(x => x.ClientId).Must(x => x >= 0).NotNull().WithMessage("Dit veld is verplicht");   
+                RuleFor(x => x.ClientId).Must(x => x >= 0).NotNull().WithMessage("Dit veld is verplicht");
+                RuleFor(x => x.Software).IsInEnum().WithMessage("Ongeldig mode");
 
             }
         }
