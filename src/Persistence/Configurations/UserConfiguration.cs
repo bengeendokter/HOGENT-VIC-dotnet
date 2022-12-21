51721a5e403630d8df2267af35849015e105c534
aglim-benli <aglim.benli@student.hogent.be>
2022-12-21 15:03:27 +0100

    Create UserConfiguration.cs

---
+++ src/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations;
+
+internal class UserConfiguration : EntityConfiguration<User>
+{
+    public override void Configure(EntityTypeBuilder<User> builder)
+    {
+        base.Configure(builder);
+    }
+}
