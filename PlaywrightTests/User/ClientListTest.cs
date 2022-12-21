dbca6e810d7c1c50d4c7227bd1de32b65958bb6a
CedrickLauwyck <cedrick.lauwyck@student.hogent.be>
2022-12-21 16:59:28 +0100

    Update playwright

---
+++ PlaywrightTests/User/ClientListTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using VIC.IntegraionTests;
+
+namespace VIC.IntegrationTests.User;
+
+[Parallelizable(ParallelScope.Self)]
+public class ClientListTest : PageTest
+{
+    [Test]
+    public async Task Show_25_Items_In_ClientTable()
+    {
+        await Page.GotoAsync($"{TestHelper.BaseUri}/klanten/");
+
+        await Page.WaitForSelectorAsync("h1");
+
+        var rows = await Page.Locator("tr").CountAsync();
+
+        // 25 Rows + header row
+        Assert.AreEqual(26, rows);
+    }
+
+    [Test]
+    public async Task Show_Only_Internal()
+    {
+        await Page.GotoAsync($"{TestHelper.BaseUri}/klanten/");
+
+        await Page.WaitForSelectorAsync("h1");
+
+    }
+}
