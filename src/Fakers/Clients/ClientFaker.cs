fc108f01a2eb1f2568bc197c64090c09745ed6b9
Ben <79012763+benartshogent@users.noreply.github.com>
2022-12-22 16:28:39 +0100

    Simplify faker

--- src/Fakers/Clients/ClientFaker.cs
+++ src/Fakers/Clients/ClientFaker.cs
@@ -21,8 +21,8 @@ public class ClientFaker : EntityFaker<Client>
                     f.Phone.PhoneNumber("+## ### ## ## ##"),          // BackupContact
                     f.Random.Enum<EClientType>(),   // ClientType
                     f.Company.CompanyName(),        // ClientOrganisation
-                    f.Random.CollectionItem<String>(new List<String>() { "Toegepaste Informatica", "Chemie", "Elektro-mechanica", "Biotechnologie" }),        // Education
-                    f.Random.CollectionItem<String>(new List<String>() { "Manager", "CEO", "Developer", "Researcher" }),        // ExternalType
+                    f.Random.CollectionItem<string>(new List<string>() { "Toegepaste Informatica", "Chemie", "Elektro-mechanica", "Biotechnologie" }),        // Education
+                    f.Random.CollectionItem<string>(new List<string>() { "Manager", "CEO", "Developer", "Researcher" }),        // ExternalType
                     f.Random.Bool()                 // IsActive
                 );
         }
